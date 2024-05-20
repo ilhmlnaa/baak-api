e397118a2df6101a9033fad7702de5f28efd805e
Ilham Maulana <poloklasik32@gmail.com>
2024-05-20 09:25:33 +0700

    update:response

--- index.js
+++ index.js
@@ -1,72 +1,69 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
 const app = express();
 const jadwalMatkul = require("./jadwalMatkul");
-const jadwalUts = require('./jadwalUts');
-const response = require('./response')
+const jadwalUts = require("./jadwalUts");
+const response = require("./response");
 
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
+app
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs");
 
-app.set('view engine','ejs'); 
-app.engine('ejs', require('ejs').__express);
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express);
 
-
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
+app.get("/matkul", async (req, res) => {
+  const kelas = req.query.kelas;
 
-
-app.get('/matkul', async (req, res) => { 
-  const kelas = req.query.kelas.split('?')[0];  
-  
   if (!kelas) {
-    return res.status(400).json({ error: 'Parameter data diperlukan.' });
+    return response(400, "Parameter Kelas Diperlukan!", "Error", res);
   }
-  
+
   const url = `https://baak.gunadarma.ac.id/jadwal/cariJadKul?teks=${kelas}`;
   try {
-    const fetchApi = await jadwalMatkul(url); 
+    const fetchApi = await jadwalMatkul(url);
 
     if (!fetchApi || Object.keys(fetchApi).length === 0) {
-      return res.status(404).json({ error: 'Data tidak ditemukan.' });
+      return res.status(404).json({ error: "Data tidak ditemukan." });
     }
 
-    res.json(fetchApi);
+    response(200, fetchApi, "Success Get Data", res);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data.' });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Terjadi kesalahan saat mengambil data." });
   }
 });
 
+app.get("/uts", async (req, res) => {
+  const kelas = req.query.kelas;
 
-app.get('/uts', async (req, res) => { 
-  const kelas = req.query.kelas.split('?')[0]; ; 
-  
   if (!kelas) {
-    return res.status(400).json({ error: 'Parameter data diperlukan.' });
+    return response(400, "Parameter Kelas Diperlukan!", "Error", res);
   }
-  
+
   const url = `https://baak.gunadarma.ac.id/jadwal/cariUts?teks=${kelas}`;
-  
+
   try {
-    const fetchApi = await jadwalUts(url); 
+    const fetchApi = await jadwalUts(url);
 
     if (!fetchApi || Object.keys(fetchApi).length === 0) {
-      return res.status(404).json({ error: 'Data tidak ditemukan.' });
+      return res.status(404).json({ error: "Data tidak ditemukan." });
     }
 
-    res.json(fetchApi);
+    response(200, fetchApi, "Success Get Data", res);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data.' });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Terjadi kesalahan saat mengambil data." });
   }
 });
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
-})
+});
